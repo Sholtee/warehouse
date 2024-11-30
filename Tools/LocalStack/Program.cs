a0ccd91c20d7b01eac3904cc7f5a19de0d780480
denes.solti <sodnaatx@gmail.com>
2024-11-30 14:54:36 +0100

    "using" cosmetics

--- Tools/LocalStack/Program.cs
+++ Tools/LocalStack/Program.cs
@@ -1,10 +1,11 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
 using Amazon.Runtime;
 using Amazon.SecretsManager;
 using Amazon.SecretsManager.Model;
 using Microsoft.AspNetCore.Identity;
-using System.Net;
-using System.Net.Http.Json;
-using System.Text.Json;
 
 using static System.Environment;
 
