61252c101f2bd4531bddc17d9a1a5fdc9573b2eb
denes.solti <sodnaatx@gmail.com>
2024-12-21 09:14:00 +0100

    move ProductOverview to a separate file

--- SRC/App/Warehouse.DAL/Repositories/WarehouseRepository/Dtos/ListProductOverviewsResult.cs
+++ SRC/App/Warehouse.DAL/Repositories/WarehouseRepository/Dtos/ListProductOverviewsResult.cs
@@ -1,25 +1,7 @@
-using System;
 using System.Collections.Generic;
 
 namespace Warehouse.DAL
 {
-    public class ProductOverview
-    {
-        public required Guid Id { get; init; }
-
-        public required string MainImage { get; init; }
-
-        public required string Name { get; init; }
-
-        public required string Brand { get; init; }
-
-        public required uint Quantity { get; init; }
-
-        public required decimal Price { get; init; }
-
-        public required DateTime ReleaseDate { get; init; }
-    }
-
     public sealed class ListProductOverviewsResult: List<ProductOverview>
     {
     }
