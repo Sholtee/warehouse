b9e8afb6df976b0840740e528b012ce35e79d2f1
denes.solti <sodnaatx@gmail.com>
2024-12-26 09:54:20 +0100

    Get-RootPw.ps1 not needed anymore

--- Get-RootPw.ps1
+++
@@ -1,9 +0,0 @@
-#
-# Get-RootPw.ps1
-#
-# Author: Denes Solti
-# Project: Warehouse API (boilerplate)
-# License: MIT
-#
-
-Write-Host (aws secretsmanager get-secret-value --secret-id local-root-user-creds --endpoint-url http://localhost:4566 --region local | ConvertFrom-Json).SecretString
