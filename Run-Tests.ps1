ed46f81a1e894c4126a427cbfddbb022ec8662b9
denes.solti <sodnaatx@gmail.com>
2024-12-28 16:16:40 +0100

    Run-Tests.ps1 cosmetics

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -14,10 +14,13 @@ $Path=[System.IO.Path]
 $artifacts=$Path::Join("$(Get-Location)", 'Artifacts')
 
 dotnet-coverage collect `
-	--settings "$($Path::Join("$(Get-Location)", 'CoverageSettings.xml'))" `
-	--output "$($Path::Join($artifacts, 'Coverage.xml'))" `
-	"dotnet test --test-adapter-path:. --logger:nunit;LogFilePath=$($Path::Join($artifacts, '{assembly}.Results.xml'))"
+  --settings $Path::Join("$(Get-Location)", 'CoverageSettings.xml') `
+  --output $Path::Join($artifacts, 'Coverage.xml') `
+  "dotnet test --test-adapter-path:. --logger:nunit;LogFilePath=$($Path::Join($artifacts, '{assembly}.Results.xml'))"
 
 dotnet tool install --global dotnet-reportgenerator-globaltool --version 5.4.1
 
-reportgenerator "-reports:$($Path::Join($artifacts, 'Coverage.xml'))" "-targetdir:$($Path::Join($artifacts, 'CoverageReport'))" -reporttypes:Html_Dark
+reportgenerator `
+  -reports:$Path::Join($artifacts, 'Coverage.xml') `
+  -targetdir:$Path::Join($artifacts, 'CoverageReport') `
+  -reporttypes:Html_Dark
