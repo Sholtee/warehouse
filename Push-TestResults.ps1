d5f5a39777120feb82047ed9dd0a7f94787d4bea
denes.solti <sodnaatx@gmail.com>
2025-01-01 09:45:17 +0100

    try to push test results

---
+++ Push-TestResults.ps1
@@ -0,0 +1,19 @@
+#
+# Push-TestResults.ps1
+#
+# Author: Denes Solti
+# Project: Warehouse API (boilerplate)
+# License: MIT
+#
+
+$ErrorActionPreference = "Stop"
+
+$PATH=[System.IO.Path]
+
+$artifacts=($PATH::Combine("$(Get-Location)", 'Artifacts') | Resolve-Path)
+$client=New-Object System.Net.WebClient
+
+Get-ChildItem -Path $PATH::Combine($artifacts, "*.xml") | foreach {
+  Write-Host "Uploading test result: $($_.Name)"
+  $client.UploadFile("https://ci.appveyor.com/api/testresults/$type/$Env:APPVEYOR_JOB_ID", $_.FullName)
+}
--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -10,17 +10,17 @@ $ErrorActionPreference = "Stop"
 
 dotnet tool install --global dotnet-coverage --version 17.13.1
 
-$Path=[System.IO.Path]
-$artifacts=$Path::Combine("$(Get-Location)", 'Artifacts')
+$PATH=[System.IO.Path]
+$artifacts=$PATH::Combine("$(Get-Location)", 'Artifacts')
 
 dotnet-coverage collect `
-  --settings $Path::Combine("$(Get-Location)", 'CoverageSettings.xml') `
-  --output $Path::Combine($artifacts, 'Coverage.xml') `
-  "dotnet test --test-adapter-path:. --logger:nunit;LogFilePath=$($Path::Combine($artifacts, '{assembly}.Results.xml'))"
+  --settings $PATH::Combine("$(Get-Location)", 'CoverageSettings.xml') `
+  --output $PATH::Combine($artifacts, 'Coverage.xml') `
+  "dotnet test --test-adapter-path:. --logger:nunit;LogFilePath=$($PATH::Combine($artifacts, '{assembly}.Results.xml'))"
 
 dotnet tool install --global dotnet-reportgenerator-globaltool --version 5.4.1
 
 reportgenerator `
-  -reports:$Path::Combine($artifacts, 'Coverage.xml') `
-  -targetdir:$Path::Combine($artifacts, 'CoverageReport') `
+  -reports:$PATH::Combine($artifacts, 'Coverage.xml') `
+  -targetdir:$PATH::Combine($artifacts, 'CoverageReport') `
   -reporttypes:Html_Dark
