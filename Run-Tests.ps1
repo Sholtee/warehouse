8313a9a87fce626212630cf364dc8fce34167671
denes.solti <sodnaatx@gmail.com>
2025-01-01 08:53:24 +0100

    Path.Join() is not supported in all pwsh versions

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -11,16 +11,16 @@ $ErrorActionPreference = "Stop"
 dotnet tool install --global dotnet-coverage --version 17.13.1
 
 $Path=[System.IO.Path]
-$artifacts=$Path::Join("$(Get-Location)", 'Artifacts')
+$artifacts=$Path::Combine("$(Get-Location)", 'Artifacts')
 
 dotnet-coverage collect `
-  --settings $Path::Join("$(Get-Location)", 'CoverageSettings.xml') `
-  --output $Path::Join($artifacts, 'Coverage.xml') `
-  "dotnet test --test-adapter-path:. --logger:nunit;LogFilePath=$($Path::Join($artifacts, '{assembly}.Results.xml'))"
+  --settings $Path::Combine("$(Get-Location)", 'CoverageSettings.xml') `
+  --output $Path::Combine($artifacts, 'Coverage.xml') `
+  "dotnet test --test-adapter-path:. --logger:nunit;LogFilePath=$($Path::Combine($artifacts, '{assembly}.Results.xml'))"
 
 dotnet tool install --global dotnet-reportgenerator-globaltool --version 5.4.1
 
 reportgenerator `
-  -reports:$Path::Join($artifacts, 'Coverage.xml') `
-  -targetdir:$Path::Join($artifacts, 'CoverageReport') `
+  -reports:$Path::Combine($artifacts, 'Coverage.xml') `
+  -targetdir:$Path::Combine($artifacts, 'CoverageReport') `
   -reporttypes:Html_Dark
