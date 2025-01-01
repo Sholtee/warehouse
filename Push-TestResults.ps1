d8fe9b0c8c92159361cee7918a080f15a715109d
denes.solti <sodnaatx@gmail.com>
2025-01-01 09:58:41 +0100

    fix pattern

--- Push-TestResults.ps1
+++ Push-TestResults.ps1
@@ -13,7 +13,7 @@ $PATH=[System.IO.Path]
 $artifacts=($PATH::Combine("$(Get-Location)", 'Artifacts') | Resolve-Path)
 $client=New-Object System.Net.WebClient
 
-Get-ChildItem -Path $PATH::Combine($artifacts, "*.xml") | foreach {
+Get-ChildItem -Path $PATH::Combine($artifacts, "*.Tests.Results.xml") | foreach {
   Write-Host "Uploading test result: $($_.Name)"
   $client.UploadFile("https://ci.appveyor.com/api/testresults/$type/$Env:APPVEYOR_JOB_ID", $_.FullName)
 }
