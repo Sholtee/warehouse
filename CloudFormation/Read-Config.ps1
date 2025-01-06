c5c83248e82f63b89cd92ccf4ba664ea84b84dc0
denes.solti <sodnaatx@gmail.com>
2025-01-06 08:26:56 +0100

    introduce Read-Config.ps1

---
+++ CloudFormation/Read-Config.ps1
@@ -0,0 +1,26 @@
+#
+# Read-Config.ps1
+#
+# Usage: Read-Config.ps1 -path path/to/config.json [-extra @{key1=value1; key2="value2"}]
+#
+# Author: Denes Solti
+# Project: Warehouse API (boilerplate)
+# License: MIT
+#
+
+param(
+  [Parameter(Position=0, Mandatory=$true)]
+  [string]$path,
+
+  [Parameter(Position=1)]
+  [HashTable]$extra = @{}
+)
+
+$ErrorActionPreference = "Stop"
+
+$config = (Get-Content $path -Raw | ConvertFrom-Json -AsHashTable) + $extra
+$config.GetEnumerator() | ForEach-Object `
+  -Begin {$params=""} `
+  -Process {$params += "`"ParameterKey=$($_.Key),ParameterValue=$($_.Value)`" "}
+
+return $params
