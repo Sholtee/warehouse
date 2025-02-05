20e34a1f226453a63d2e5ac4fe4515f588491ac2
denes.solti <sodnaatx@gmail.com>
2025-02-05 18:26:53 +0100

    profile db connection

--- SRC/App/Warehouse.Host/Services/MySqlConnectionFactory.cs
+++ SRC/App/Warehouse.Host/Services/MySqlConnectionFactory.cs
@@ -18,6 +18,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MySqlConnector;
+using StackExchange.Profiling;
+using StackExchange.Profiling.Data;
 
 namespace Warehouse.Host.Services
 {
@@ -86,7 +88,13 @@ namespace Warehouse.Host.Services
         {
             DbConnection connection = DataSource.CreateConnection();
             connection.Open();
-            return connection;
+
+            //
+            // ProfiledDbConnection does nothing if the underlying profiler is not active
+            // (MiniProfiler.Current != null)
+            //
+
+            return new ProfiledDbConnection(connection, MiniProfiler.Current);
         }
     }
 }
